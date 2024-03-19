a5e28dbe79de6235250f2f406ca05854440ccc4e
Burhan <alpagut1993@gmail.com>
2024-03-19 11:15:45 +0100

    db connection

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,6 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   // Routes that can be accessed while signed out
   publicRoutes: ["/"],
-  ignoredRoutes: ["/"],
 });
 
 export const config = {
