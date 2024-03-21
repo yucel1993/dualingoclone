1b3685f9503beb351d4feb228857df28eb4d21d4
Burhan <alpagut1993@gmail.com>
2024-03-21 09:44:24 +0100

    user-progress:applyed

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -1,13 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
 import { InfinityIcon } from "lucide-react";
-
-// import { courses } from "@/db/schema";
+import { courses } from "@/db/schema";
 import { Button } from "@/components/ui/button";
 
 type Props = {
-  //   activeCourse: typeof courses.$inferSelect;
-  activeCourse: any;
+  activeCourse: typeof courses.$inferSelect;
   hearts: number;
   points: number;
   hasActiveSubscription: boolean;
